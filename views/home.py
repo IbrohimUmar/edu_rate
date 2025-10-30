43a9d376368d8a53d07925f211b2c28c79e45500
Ibrohim Umarxo'jayev <IbrohimUmarr@gmail.com>
2025-10-30 12:18:30 +0500

    a

--- views/home.py
+++ views/home.py
@@ -24,64 +24,64 @@ def home(request):
     )
 
 
-
-    from django.utils import timezone
-    from datetime import timedelta
-
-    today = timezone.now()
-    three_days_ago = today - timedelta(days=3)
-    schedules = Schedule.objects.filter(lesson_date__gte=three_days_ago)
-    print(schedules.count())
-    from django.utils import timezone
-
-    random_number = random.randint(1, 10)
-    print(random_number)
-    ta = schedules.count()
-    for schedule in schedules:
-        ta -=1
-        print(ta)
-        random_number = random.randint(1,10)
-        if random_number > 3:
-            students = StudentMeta.objects.filter(group=schedule.group)
-            for s in students:
-                random_fot_st = random.randint(1,10)
-                if random_fot_st <= 2:
-                    SchedulePoint.objects.create(
-                        student=s.user,
-                        schedule=schedule,
-                        employee=schedule.employee,
-                        submission_deadline=calculate_deadline(
-                                schedule.lesson_date, schedule.lesson_pair.start_time
-                            ),
-                        notification_planned_date=calculate_lesson_end_time(
-                                schedule.lesson_date, schedule.lesson_pair.end_time
-                            ),
-                        is_teacher_present='2',
-                        notification_sent_at=timezone.now(),
-                        answer_submitted_at=timezone.now()
-                    )
-                elif random_fot_st == '3':
-                    continue
-                else:
-
-                    SchedulePoint.objects.create(
-                        student=s.user,
-                        schedule=schedule,
-                        employee=schedule.employee,
-                        submission_deadline=calculate_deadline(
-                                schedule.lesson_date, schedule.lesson_pair.start_time
-                            ),
-                        notification_planned_date=calculate_lesson_end_time(
-                                schedule.lesson_date, schedule.lesson_pair.end_time
-                            ),
-                        is_teacher_present='1',
-                        teacher_speech_and_culture=str(random.randint(1,3)),
-                        topic_practical_relevance=str(random.randint(1,2)),
-                        lesson_feedback=str(random.randint(1,3)),
-
-                        notification_sent_at=timezone.now(),
-                        answer_submitted_at=timezone.now()
-                    )
+    #
+    # from django.utils import timezone
+    # from datetime import timedelta
+    #
+    # today = timezone.now()
+    # three_days_ago = today - timedelta(days=3)
+    # schedules = Schedule.objects.filter(lesson_date__gte=three_days_ago)
+    # print(schedules.count())
+    # from django.utils import timezone
+    #
+    # random_number = random.randint(1, 10)
+    # print(random_number)
+    # ta = schedules.count()
+    # for schedule in schedules:
+    #     ta -=1
+    #     print(ta)
+    #     random_number = random.randint(1,10)
+    #     if random_number > 3:
+    #         students = StudentMeta.objects.filter(group=schedule.group)
+    #         for s in students:
+    #             random_fot_st = random.randint(1,10)
+    #             if random_fot_st <= 2:
+    #                 SchedulePoint.objects.create(
+    #                     student=s.user,
+    #                     schedule=schedule,
+    #                     employee=schedule.employee,
+    #                     submission_deadline=calculate_deadline(
+    #                             schedule.lesson_date, schedule.lesson_pair.start_time
+    #                         ),
+    #                     notification_planned_date=calculate_lesson_end_time(
+    #                             schedule.lesson_date, schedule.lesson_pair.end_time
+    #                         ),
+    #                     is_teacher_present='2',
+    #                     notification_sent_at=timezone.now(),
+    #                     answer_submitted_at=timezone.now()
+    #                 )
+    #             elif random_fot_st == '3':
+    #                 continue
+    #             else:
+    #
+    #                 SchedulePoint.objects.create(
+    #                     student=s.user,
+    #                     schedule=schedule,
+    #                     employee=schedule.employee,
+    #                     submission_deadline=calculate_deadline(
+    #                             schedule.lesson_date, schedule.lesson_pair.start_time
+    #                         ),
+    #                     notification_planned_date=calculate_lesson_end_time(
+    #                             schedule.lesson_date, schedule.lesson_pair.end_time
+    #                         ),
+    #                     is_teacher_present='1',
+    #                     teacher_speech_and_culture=str(random.randint(1,3)),
+    #                     topic_practical_relevance=str(random.randint(1,2)),
+    #                     lesson_feedback=str(random.randint(1,3)),
+    #
+    #                     notification_sent_at=timezone.now(),
+    #                     answer_submitted_at=timezone.now()
+    #                 )
 
 
 
