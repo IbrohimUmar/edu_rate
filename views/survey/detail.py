d9331b66cc6b2e1f894fc66c8dba3be4eae00a21
Ibrohim Umarxo'jayev <76086219+IbrohimUmar@users.noreply.github.com>
2025-11-01 16:01:39 +0500

    a

--- views/survey/detail.py
+++ views/survey/detail.py
@@ -6,4 +6,8 @@ from models.models.meta import EducationType
 @login_required(login_url='login')
 def survey_detail(request, id):
     education_type = get_object_or_404(EducationType, id=id)
+
+    if request.method == 'POST':
+        print(request.POST)
+
     return render(request, "survey/detail.html", {"education_type":education_type})
