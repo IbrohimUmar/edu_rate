11a4f732cf727e8afa20731cb5e510a530bd9c79
Ibrohim Umarxo'jayev <76086219+IbrohimUmar@users.noreply.github.com>
2025-12-05 17:35:15 +0500

    asd

--- views/welcome_student.py
+++ views/welcome_student.py
@@ -5,6 +5,6 @@ from models.models.student_meta import StudentMeta
 
 @login_required(login_url='login')
 def welcome_student(request):
-    student_meta = StudentMeta.objects.filter(student_id=request.user.id)
+    student_meta = StudentMeta.objects.filter(user_id=request.user.id)
     return render(request, 'welcome_student.html', {
         "student_meta":student_meta})
